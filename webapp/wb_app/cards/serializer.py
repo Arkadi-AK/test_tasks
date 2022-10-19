50f97e92c6bf4d09dd512ef2146554b311c5066b
Arkadi-AK <akpy375@gmail.com>
2022-10-19 10:00:21 +0300

    change serializer

--- webapp/wb_app/cards/serializer.py
+++ webapp/wb_app/cards/serializer.py
@@ -2,14 +2,6 @@ from django.core.validators import FileExtensionValidator
 from rest_framework.serializers import FileField, Serializer, IntegerField
 
 
-class UploadExelSerializer(Serializer):
-    file = FileField(validators=[FileExtensionValidator(allowed_extensions=["xlsx"])])
-    article = IntegerField()
-
-    class Meta:
-        fields = ["file", "article"]
-
-
 class ArticleSerializer(Serializer):
     article = IntegerField()
 
